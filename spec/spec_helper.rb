8965e1042b7e0f1fe0211f3de6185fb8736a2b70
Drew Dara-Abrams <drew@mapzen.com>
2015-03-04 16:18:33 -0800

    updating VCR configuration to current version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,12 +9,10 @@ SimpleCov.start do
   add_filter 'spec' # ignore spec files
 end
 
-RSpec.configure do |config|
-  config.extend VCR::RSpec::Macros
-end
 require 'onestop_id_client'
 
 VCR.configure do |c|
   c.cassette_library_dir = File.join(File.dirname(__FILE__), '/test_data/vcr_cassettes')
   c.hook_into :webmock
+  c.configure_rspec_metadata!
 end
