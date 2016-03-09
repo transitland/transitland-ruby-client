01fa2afac691f37d3121bf9a736daf503432ef4f
tgreen8091 <tgreen8091@gmail.com>
2016-03-09 05:08:23 +0000

    Move common functionality from Feed to Entity

--- lib/transitland_client/entities/feed.rb
+++ lib/transitland_client/entities/feed.rb
@@ -2,46 +2,6 @@ require 'json'
 
 module TransitlandClient
   class Feed < Entity
-
-    def initialize(json)
-      map_json(json)
-    end
-
-    def self.handle_caching(options)
-      if entity = TransitlandClient::Cache.get_entity(options[:onestop_id])
-        return entity
-      else
-        yield.tap do |response|
-          TransitlandClient::Cache.set_entity(options[:onestop_id],response)
-          return response
-        end
-      end
-    end
-
-    def self.find_by(options={})
-      found_objects = []
-      response = nil
-      response = handle_caching(options) do
-        HTTParty.get(base_path + "feeds", query: options).body
-      end
-      parsed_json = JSON.parse(response)
-      parsed_json["feeds"].each do |feed|
-        found_objects << new(feed)
-      end
-
-      if options[:onestop_id]
-        return found_objects.first
-      else
-        return found_objects
-      end
-    end
-
-    def map_json(json)
-      json.each do |key, value|
-        instance_variable_set("@#{key}", json[key])
-        self.class.class_eval { attr_reader key.to_sym }
-      end
-    end
   end
 end
 =begin
--- lib/transitland_client/entities/operator_in_feed.rb
+++ lib/transitland_client/entities/operator_in_feed.rb
@@ -1,24 +1,4 @@
-require_relative 'feed'
-# require_relative 'operator'
-
 module TransitlandClient
-    class OperatorInFeed
-      attr_accessor :gtfs_agency_id, :operator_onestop_id, :feed_onestop_id, :feed, :identifiers
-
-      def initialize(gtfs_agency_id: nil, operator_onestop_id: nil, feed_onestop_id: nil, feed: nil, identifiers: nil)
-        if feed
-          @feed = feed
-        elsif feed_onestop_id
-          @feed = Feed.new(onestop_id: feed_onestop_id)
-        else
-          raise ArgumentError.new('a feed object or Onestop ID must be specified')
-        end
-
-        @gtfs_agency_id = gtfs_agency_id
-        @operator_onestop_id = operator_onestop_id
-        @identifiers = identifiers || []
-
-        self
-      end
-    end
+  class Operator < Entity
+  end
 end
